7d67e1739a55fcd0d68ca649f24a95871d034b4f
Tamarozi <cauaptamarozi@gmail.com>
2025-05-07 19:45:53 -0300

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[5] = {9, 7, 3, 6, 8};
+    int n = 5;
+
+    printf("Antes: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                int maior = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = maior;
+            }
+        }
+    }
+
+    printf("Depois: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
