be0af3f39f1006781e204cb545d0f94ab30705ef
Tamarozi <cauaptamarozi@gmail.com>
2025-05-06 22:01:47 -0300

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char palavra[11];
+
+    printf("Digite uma palavra de até 10 letras: ");
+    scanf("%10s", palavra);
+
+    
+
+        printf("É um palíndromo\n");
+    } else {
+        printf("Não é um palíndromo.\n");
+    }
+    return 0; 
+}
